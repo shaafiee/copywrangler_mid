c4743c82272c2eff1dd35c0ed313f3af4845e36e
shaafiee <shaafiee@gmail.com>
2024-06-19 17:47:35 +0800

    refresh

--- data/base.py
+++ data/base.py
@@ -16,16 +16,19 @@ class PullScope(BaseModel):
 	session: str
 	pages: int | None = 0
 	collections: int | None = 0
+	assets: int | None = 0
 	scope: str | None = None
 
 class PullTransScope(BaseModel):
 	session: str
 	resource: int | None = 0
+	resourceType: int | None = 1
 	scope: str | None = None
 
 class UpdateTrans(BaseModel):
 	session: str
 	resource: int
+	resourceType: int | None = 1
 	trKey: str
 	trValue: str
 	lang: str
