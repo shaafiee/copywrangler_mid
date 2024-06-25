a82ac53a2b5b2b4a8a93771ab8d20aa1eb42a75d
Shaafiee <mohamed.shaafiee@comfort-works.com>
2024-06-25 20:40:00 +0000

    refresh

--- app.py
+++ app.py
@@ -308,7 +308,7 @@ def compileCSV(rows, isColl = False, isAsset = False):
 				for aValue in current[len(theLangs) * -1:]:
 					if len(aValue) > 2:
 						totalValues = totalValues + 1
-				if totalValues < len(theLangs) and 'en' in keyLang.keys() and validValue(keyLang['en']):
+				if totalValues < len(theLangs) - 1 and 'en' in keyLang.keys() and validValue(keyLang['en']):
 					if isColl:
 						if not re.search(r"^[A-Za-z0-9]+(\-|_)[A-Za-z0-9]+((\-|_)[A-Za-z0-9]+)*", keyLang['en']):
 							body.append(current)
