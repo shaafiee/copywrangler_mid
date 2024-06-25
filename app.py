a738e6dbefd6672a1d6e589a28fcae77962a6330
Shaafiee <mohamed.shaafiee@comfort-works.com>
2024-06-25 21:08:08 +0000

    refresh

--- app.py
+++ app.py
@@ -306,7 +306,7 @@ def compileCSV(rows, isColl = False, isAsset = False):
 				#preJoin = ','.join(current)
 				totalValues = 0
 				for idx, aValue in enumerate(current):
-					if idx > 2 and (len(aValue) < 2 or aValue == ""):
+					if idx > 2 and (not aValue or aValue == ""):
 						totalValues = totalValues + 1
 				if 'en' in keyLang.keys() and validValue(keyLang['en']):
 					if isColl:
